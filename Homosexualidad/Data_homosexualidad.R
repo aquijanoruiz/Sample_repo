7cf8b43bec816dce42810d0846744d2fd5bcc1a0
Alonso Quijano <aquijanoruiz@gmail.com>
2022-02-22 11:45:01 -0500

    cambios al branch

--- Homosexualidad/Data_homosexualidad.R
+++ Homosexualidad/Data_homosexualidad.R
@@ -2,6 +2,8 @@
 
 # paquetes
 library(foreign)
+library(tidyverse)
+# cambios
 
 # base de datos
 WVS <- read.dta("https://github.com/aquijanoruiz/elquantificador_posts/raw/master/valores/2020-11-02-los-ecuatorianos-aprueban-el-aborto/WV6_Data_Ecuador_2013_Stata_v20180912.dta")
